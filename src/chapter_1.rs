a455a10b395065d6547f5a37849c7723076d6234
Marcus Willock <crazcalm@gmail.com>
2021-06-10 20:00:51 -0400

    Add unit tests for chapter 1 stuff

--- src/chapter_1.rs
+++ src/chapter_1.rs
@@ -1,11 +1,4 @@
-fn count(arg: &[u32]) {
-    println!("{:#?}", arg);
-
-    if let Some((_, elements)) = arg.split_first() {
-        println!("{:#?}", elements);
-    }
-}
-
+#[allow(dead_code)]
 fn list_length<T>(args: &[T]) -> u32 {
     match args.is_empty() {
         true => 0,
@@ -16,6 +9,7 @@ fn list_length<T>(args: &[T]) -> u32 {
     }
 }
 
+#[allow(dead_code)]
 fn find_index<T: PartialEq + Clone>(args: &[T], target: T) -> i32 {
     match in_list(args, target.clone()) {
         false => -1,
@@ -31,6 +25,7 @@ fn find_index<T: PartialEq + Clone>(args: &[T], target: T) -> i32 {
     }
 }
 
+#[allow(dead_code)]
 fn nth_element<T>(arg: &[T], count: u32) -> &T {
     if list_length(arg) - 1 < count {
         panic!("splice does not have {} elements", count)
@@ -48,6 +43,7 @@ fn nth_element<T>(arg: &[T], count: u32) -> &T {
     }
 }
 
+#[allow(dead_code)]
 fn remove_first<T: Clone + PartialEq>(arg: &[T], target: T) -> Vec<T> {
     let index = find_index(arg, target);
     if index == -1 {
@@ -61,6 +57,7 @@ fn remove_first<T: Clone + PartialEq>(arg: &[T], target: T) -> Vec<T> {
     }
 }
 
+#[allow(dead_code)]
 fn remove_all(arg: &[u32], target: u32) -> Vec<u32> {
     match arg.to_vec() == remove_first(arg, target) {
         true => arg.to_vec(),
@@ -71,6 +68,7 @@ fn remove_all(arg: &[u32], target: u32) -> Vec<u32> {
     }
 }
 
+#[allow(dead_code)]
 fn in_list<T: Clone + PartialEq>(arg: &[T], target: T) -> bool {
     match arg.is_empty() {
         true => false,
@@ -86,18 +84,84 @@ fn in_list<T: Clone + PartialEq>(arg: &[T], target: T) -> bool {
     }
 }
 
-//TODO: convert this to unit tests
-fn main() {
-    let test = vec![1, 2, 3, 4, 5, 4, 2, 4, 6];
-    println!("{}", list_length(&test[..]));
+#[cfg(test)]
+mod tests {
+    use super::*;
 
-    println!("{}", nth_element(&test[..], 3));
+    #[test]
+    fn test_find_index() {
+        let list = vec![5, 4, 3, 2, 1];
 
-    println!("{:?}", in_list(&test[..], 4));
+        // Happy path
+        assert_eq!(find_index(&list[..], 2), 3);
 
-    println!("find_index {:?}", find_index(&test[..], 16));
+        // Less happy path
+        assert_eq!(find_index(&list[..], 8), -1);
+    }
 
-    println!("remove_first {:?}", remove_first(&test[..], 4));
-    println!("{:?}", remove_all(&test[..], 4));
-    println!("{}", vec![1, 2] == vec![1, 2])
+    #[test]
+    fn test_list_length() {
+        let list = vec![1, 2, 3, 4, 5, 6, 7];
+
+        // empty list
+        let mut empty_list = vec![1];
+        let _ = empty_list.pop();
+
+        assert_eq!(list_length(&list[..]), 7);
+        assert_eq!(list_length(&empty_list[..]), 0);
+    }
+
+    #[test]
+    #[should_panic(expected = "splice does not have 8 elements")]
+    fn test_nth_element_panic() {
+        let list = vec![1, 2, 3];
+
+        nth_element(&list[..], 8);
+    }
+
+    #[test]
+    fn test_nth_element() {
+        let list = vec![5, 4, 3, 2, 1, 0];
+
+        // Happy path
+        assert_eq!(nth_element(&list, 3), &list[3]);
+    }
+
+    #[test]
+    fn test_remove_all() {
+        let list = vec![1, 2, 3, 1, 4, 1];
+
+        // Happy path
+        assert_eq!(remove_all(&list[..], 1), vec![2, 3, 4]);
+        assert_eq!(remove_all(&list[..], 2), vec![1, 3, 1, 4, 1]);
+
+        // Less Happy path
+        assert_eq!(remove_all(&list[..], 5), list);
+    }
+
+    #[test]
+    fn test_remove_first() {
+        let list = vec![1, 2, 3];
+
+        // Happy path
+        assert_eq!(remove_first(&list[..], 1), vec![2, 3]);
+        assert_eq!(remove_first(&list[..], 2), vec![1, 3]);
+
+        // Less happy path
+        assert_eq!(remove_first(&list[..], 5), vec![1, 2, 3]);
+    }
+
+    #[test]
+    fn test_in_list() {
+        let list = vec![1, 2, 3];
+
+        // Happy path
+        assert_eq!(in_list(&list[..], 1), true);
+        assert_eq!(in_list(&list[..], 2), true);
+        assert_eq!(in_list(&list[..], 3), true);
+
+        // less happy path
+        assert_eq!(in_list(&list[..], 0), false);
+        assert_eq!(in_list(&list[..], 4), false);
+    }
 }
